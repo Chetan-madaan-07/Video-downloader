08252461df0601b8872465bfc0a653b20fb24d8e
Chetan-madaan-07 <chetanmadaan903@gmail.com>
2025-07-12 20:25:30 +0530

    Initial commit - Navbar ready

---
+++ script.js
@@ -0,0 +1,5 @@
+function toggleMenu() {
+  document.getElementById("navMenu").classList.toggle("show");
+  document.getElementById("navBtns").classList.toggle("show");
+  document.querySelector(".navbar").classList.toggle("no-shadow")
+}
