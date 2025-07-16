21d8468d46cbd49e950a9fecbf09c072da084127
Chetan-madaan-07 <chetanmadaan903@gmail.com>
2025-07-16 20:12:26 +0530

    started working on main webpage

--- script.js
+++ script.js
@@ -3,3 +3,10 @@ function toggleMenu() {
   document.getElementById("navBtns").classList.toggle("show");
   document.querySelector(".navbar").classList.toggle("no-shadow")
 }
+function toggleDropdown() {
+  const list = document.getElementById("formatList");
+  list.style.display = list.style.display === "flex" ? "none" : "flex";
+
+  const icon = document.querySelector(".more-header i");
+  icon.style.transform = list.style.display === "flex" ? "rotate(180deg)" : "rotate(0)";
+}
