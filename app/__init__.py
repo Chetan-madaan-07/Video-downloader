ba9b9a715966180bb91554a4b14a9c6b90aba974
chetan <chetanmadaan903@gmail.com>
2025-12-19 17:36:20 +0530

    finished

--- app/__init__.py
+++ app/__init__.py
@@ -1,5 +1,3 @@
-# File: app/__init__.py
-
 from flask import Flask
 from flask_cors import CORS
 
@@ -7,14 +5,15 @@ def create_app():
     app = Flask(__name__)
     CORS(app)
 
-    # Dono blueprints ko import karo
     from .routes import views, main
-
-    # Dono blueprints ko app ke saath register karo
     app.register_blueprint(views, url_prefix='/')
-    app.register_blueprint(main, url_prefix='/api') # API routes ko /api/ ke andar daalna acchi practice hai
+    app.register_blueprint(main, url_prefix='/api')
 
     return app
+
+#  THIS LINE IS IMPORTANT
+app = create_app()
+
 
 
 
